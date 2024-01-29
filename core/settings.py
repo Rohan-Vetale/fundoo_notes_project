34a217558e40b7858cfc45aef10994bfbe36f732
Rohan-Vetale <60183607+Rohan-Vetale@users.noreply.github.com>
2024-01-29 12:01:52 +0530

    changed setting py

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,12 @@
-DATABASE_NAME = "fundoo_notes"
-DATABASE_PASSWORD = "123456"
-DATABASE_DIALECT = "postgresql"
-DATABASE_DRIVER = "psycopg2"
-DATABASE_USERNAME = "postgres"
-HOST = "localhost"
-DEFAULT_PORT = "5432"
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
+
+DATABASE_NAME : str = os.getenv('DATABASE_NAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_DIALECT = os.getenv('DATABASE_DIALECT')
+DATABASE_DRIVER = os.getenv('DATABASE_DRIVER')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+HOST = os.getenv('HOST')
+DEFAULT_PORT = os.getenv('DEFAULT_PORT')
