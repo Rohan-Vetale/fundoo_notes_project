ab3f707f7a9662a8d49a8ed541606bec26eece9c
rohan vetale <vetalerohan@gmail.com>
2024-02-01 11:33:48 +0530

    [Rohan] Add: Added the view full note api

--- routes/notes.py
+++ routes/notes.py
@@ -33,7 +33,7 @@ def create_note(payload: UserNotes, request: Request, response: Response, db: Se
     
     
 @router_notes.get(path='/get_all_notes/', status_code=status.HTTP_200_OK)
-def read_notes(request: Request, db: Session = Depends(get_db)):
+def read_all_notes(request: Request, db: Session = Depends(get_db)):
     """
     Description: This function is used to get all the notes titles of a user
     Parameter: response as Response object, db as database session.
@@ -45,3 +45,27 @@ def read_notes(request: Request, db: Session = Depends(get_db)):
     except Exception as e:
         print(e)
         return{"message": str(e)}
+    
+    
+@router_notes.get(path='/view_full_note/{note_id}', status_code=status.HTTP_200_OK)
+def view_full_note(note_id : int, request: Request,response:Response, db: Session = Depends(get_db)):
+    """
+    Description: This function is used to get all the notes titles of a user
+    Parameter: response as Response object, db as database session.
+    Return: Note titles of that user
+    """
+    try:
+        note_info = db.query(Notes).filter_by(user_id=request.state.user.id, id = note_id).one_or_none()
+
+        if note_info is None:
+            print(note_info)
+            raise HTTPException(detail= "Check the id provided and try again !",status_code=status.HTTP_404_NOT_FOUND)
+        if note_info:
+            note_title = "Note title : " + note_info.title + " , "
+            note_description = " Note description : " + note_info.description + " , "
+            note_color = " Note color : " + note_info.color 
+            complete_note = note_title + note_description + note_color
+            return{"message": f"Complete note --> {complete_note}"}
+    except Exception as e:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return{"message": "Check the note id and try again","status":400}
